e65d54ae2443a7ce8caa614d2ddec38d3d36e1e9
Icham Jaber <ijaber@student.42.fr>
2024-08-06 16:26:11 +0200

    🧙v_final

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:20:26 by ijaber            #+#    #+#             */
-/*   Updated: 2024/08/06 13:50:07 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/06 16:08:03 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, char **av)
 	fill_stack(&stack_a, tmp_stack, ac);
 	check_doublon(&stack_a);
 	init_data(&stack_a, &data);
-	if (ft_stacksize(stack_a) > 3)
+	if (ft_stacksize(stack_a) > 3 && !checksorted(stack_a))
 		big_sort(&stack_a, &stack_b, &data);
 	else
 		sort_a_min(&stack_a, &data);
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 14:57:12 by ijaber            #+#    #+#             */
-/*   Updated: 2024/07/10 19:13:04 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/08/06 16:24:58 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	**check_args(int ac, char **av)
 	i = 1;
 	check_correct(av + 1);
 	if (ac < 2)
-		push_swap_error("No arguments");
+		exit(0);
 	if (ac == 2)
 	{
 		return (splitted = ft_split(av[1], ' '));
