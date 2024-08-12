ea7addf17cd51f4aeeafa9e13e27bf67afc0a2f1
Icham Jaber <ijaber@student.42.fr>
2024-08-12 21:00:53 +0200

    update

---
+++ Libft/libft/srcs/ft_strcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 19:38:06 by ijaber            #+#    #+#             */
+/*   Updated: 2024/08/11 19:38:15 by ijaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	index;
+
+	index = 0;
+	while (s1[index] != '\0' || s2[index] != '\0')
+	{
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		index++;
+	}
+	return (0);
+}
